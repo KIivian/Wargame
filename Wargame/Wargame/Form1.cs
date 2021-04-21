61da6d59c2a5cec8f383b56f71c7ba1e2362d356
KIivian <82084552+KIivian@users.noreply.github.com>
2021-04-21 17:26:19 +0500

    Add files via upload

--- Wargame/Wargame/Form1.cs
+++ Wargame/Wargame/Form1.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Windows.Shapes;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Wargame
 {
-    public partial class Form1 : Form
+
+    public partial class MapForm : Form
     {
-        public Form1()
+        private PointF[] GetHexCorners(PointF center,double size)
         {
+            var corners = new PointF[6];
+            for (var i = 0; i < 6; i++)
+            {
+                var angle_deg = 60 * i + 30;
+                var angle_rad = Math.PI / 180 * angle_deg;
+                corners[i].X = (float)(center.X + size * Math.Cos(angle_rad));
+                corners[i].Y = (float)(center.Y + size * Math.Sin(angle_rad));
+        }
+            return corners;
+        }
+        public MapForm()
+        {
+            var hexagon = new Polyg
             InitializeComponent();
         }
 
